f71753d975d414b2c8944827c9ca87cc4a073976
Francisney Henrique <francisneyh@gmail.com>
2022-07-15 16:41:29 -0300

    js interação

---
+++ Módulo VII - JavaScript/index.js
@@ -0,0 +1,8 @@
+/*alert('Olá, seja bem vindo');
+document.write('Essa é nossa primeira vídeo aula');*/
+ 
+document.querySelector('#t2').textContent = 'Exemplificando algumas funcionalidades';
+
+const nome = prompt('Qual é o seu nome ?');
+
+alert(`Que bom ter você aqui ${nome}! \n Vamos começar?`);
