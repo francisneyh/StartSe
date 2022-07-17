d9fece4e6988299b268fe67c1b77d69a967fbfd7
Francisney Henrique <francisneyh@gmail.com>
2022-07-16 22:43:08 -0300

    up operadores1

---
+++ Módulo VII - JavaScript/operadores1.js
@@ -0,0 +1,80 @@
+//OPERADORES ARITMETICOS
+
+// + adicao
+let a = 10, b = 2;
+let adicao = a + b;
+console.log (adicao);
+
+// - subtracao
+let subtracao = a - b;
+console.log (subtracao);
+
+// * multiplicacao
+let multiplicacao = a * b;
+console.log (multiplicacao);
+
+// / divisao
+let divisao = a / b;
+console.log (divisao);
+
+// % resto
+console.log(21 % 4);
+
+// * exponenciacao
+console.log(4 ** 2);
+
+
+
+//OPERADORES DE ATRIBUICAO
+// = atribuicao
+let cor = 'amarelo';
+let numero = 10;
+console.log(cor);
+console.log(numero);
+
+// *= atribui e multiplica o valor
+let num = 3;
+num *= 2;
+console.log(num);
+
+// **= atribui e faz a exponencia do valor
+console.log(num **= 2);
+
+// /= atribui e multiplica o valor
+console.log(num *= 10);
+
+// += atribui e soma o valor
+console.log(num += 100);
+
+// -= atribui e subtrai o valor
+console.log(num -= 300);
+
+
+
+//OPERADORES RELACIONAIS
+let x = 10;
+let y = 20;
+
+// > maior
+console.log(x > 10);
+
+// < menor
+console.log(x < 10);
+
+// >= maior ou igual
+console.log(x >= y);
+
+// <menor ou igual
+console.log(x <= y);
+
+
+
+//OPERADORES DE IGUAL 
+
+// == NAO CONSIDERA O TIPO (SE E NUMERO OU STRING)
+let nota1 = 5;
+let nota2 = '5';
+console.log(nota1 == nota2);
+
+// === CONSIDERA O TIPO
+console.log(nota1 == nota2);
