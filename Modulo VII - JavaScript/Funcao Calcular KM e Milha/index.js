78efa5cb03216f8289200d9ab1dd1a472311e13d
Francisney Henrique Balbino Ferreira <50467677+francisneyh@users.noreply.github.com>
2022-07-20 20:31:34 -0300

    Delete Modulo VII - JavaScript directory

--- Modulo VII - JavaScript/Funcao Calcular KM e Milha/index.js
+++
@@ -1,6 +0,0 @@
-// ESCOPO DE VARIAVEIS E CONSTANTES
-
-function calcularKmMilha(){
-    let km = document.querySelector('#valorKm').value;
-    document.querySelector('#resultado').textContent = km * 1.609;
-}
