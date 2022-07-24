c72cfd796afed2770a1a41c265a9748f099ebb8a
Francisney Henrique <francisneyh@gmail.com>
2022-07-24 13:07:10 -0300

    #1 criada a funcao de exibir/ocultar a caixa de opcoes. #2 criado o evento para marcar/desmarcar e limpar a lista de tarefas

--- Módulo VII - JavaScript/DOM/1 - Lista de Tarefas/index.js
+++ Módulo VII - JavaScript/DOM/1 - Lista de Tarefas/index.js
@@ -9,6 +9,7 @@ botaoAdicionar.addEventListener('click', function() {
     caixaTexto.value = '';
     listaTarefas.appendChild(adicionaTarefa(textoDaTarefa)); 
     // console.log(listaTarefas);   
+    exibeOcultaListaSuspensa(); // CHAMA A FUNCAO QUE EXIBE A CAIXA DE OPCOES DEPOIS QUE ADICIONA UM ITEM
     caixaTexto.focus();
 });
 
@@ -51,9 +52,35 @@ function adicionaBotaoRemover(){
     // LISTENER - SEMPRE O BOTAO REMOVER FOR CLICADO PELO MOUSE
     // REMOVE UM ITEM DA LISTA
     botaoRemover.addEventListener('click', function(){
-        listaTarefas.removeChild(this.parentNode);
+            listaTarefas.removeChild(this.parentNode);
+            exibeOcultaListaSuspensa();  // CHAMA A FUNCAO QUE OCULTA A CAIXA DE OPCOES DEPOIS QUE REMOVE TODOS OS ITENS
         }
     );
     
     return botaoRemover;
 }
+
+// EXIBE E OCULTA A CAIXA DE OPCOES 
+function exibeOcultaListaSuspensa(){
+    const elementoSPAN = document.querySelector('#tarefa');
+    if(elementoSPAN === null) {
+        listaSuspensa.setAttribute('hidden', 'hidden');
+    } else {
+        listaSuspensa.removeAttribute('hidden');
+    }
+}
+
+// MARCA/DESMARCA TODOS ELEMENTOS E LIMPA A LISTA DE TAREFAS
+listaSuspensa.addEventListener('change', function(){
+    if(listaSuspensa.selectedIndex === 1 || listaSuspensa.selectedIndex === 2) {
+        const vetorTarefas = listaTarefas.querySelectorAll('#tarefa');
+        for(tarefa of vetorTarefas) {
+            tarefa.dispatchEvent(new Event('click'));
+        }
+    } else if(listaSuspensa.selectedIndex === 3) {
+        const vetorBotoes = listaTarefas.querySelectorAll('.remover');
+        for(botao of vetorBotoes) {
+            botao.dispatchEvent(new Event('click'));
+        }
+    }
+});
